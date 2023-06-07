53cbeefa9743deabb4cefe704ab884d321eaee7f
Pandax19 <ajwhitley@comcast.net>
2023-06-06 22:01:55 -0400

    added seeds.sql info partial

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO role (title, salary, department_id)
+VALUES 
+     ("Sales Lead", 120000, 4),
+     ("Salesperson", 10000, 4),
+     ("Lead Engineer", 120000, 4),
+     ("Software Engineer ", 120000, 4),
+     ("Account Manager ", 120000, 4),
+     ("Accountant", 120000, 4),
+     ("Legal Team Lead", 120000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id )
+VALUES 
+    ("John", "Doe", 1, NULL),
+    ("Mike", "Chan", 2, 1),
+    ("Ashley", "Rodriguez", 4, NULL),
+    ("Kevin", "Tupik", 1, NULL),
+    ("Kunal," "Singh", 1, NULL),
+    ("Malia", "Brown", 1, NULL),
+    ("Sarah", "Lourd", 1, NULL),
+    ("Tom", "Allen", 1, NULL),
