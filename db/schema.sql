c5797e3d218fb39e3137518ef1b3be3db8c2ba70
Pandax19 <ajwhitley@comcast.net>
2023-06-08 21:58:43 -0400

    added other tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -11,5 +11,20 @@ CREATE TABLE employees (
  role_id INT not null,
  PRIMARY KEY (id),
  FOREIGN KEY (role_id) REFERENCES roles(id)
-)
+);
+
+CREATE TABLE roles(
+    id INT not null auto_increment,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE department(
+    id INT not null auto_increment,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
 
